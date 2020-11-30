2e0bf9523247068ee1f00d90c6b7576267f200bf
Oleh Onunfryk <olehonufryk@gmail.com>
2020-11-30 19:54:11 +0200

    Removed asteriks

--- src/main/java/com/feedback/repo/entity/FeedbackRequest.java
+++ src/main/java/com/feedback/repo/entity/FeedbackRequest.java
@@ -5,7 +5,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 import java.util.Set;
 
