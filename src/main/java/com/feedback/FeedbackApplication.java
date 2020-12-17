559b0bf42c55aa08b52bc3491e7efdf2df53743f
Viktor Kukuruza <kukuruzavictor@gmail.com>
2020-12-17 18:06:33 +0200

    moved configuration @Bean to main @SpringBootApplication class

--- src/main/java/com/feedback/FeedbackApplication.java
+++ src/main/java/com/feedback/FeedbackApplication.java
@@ -2,12 +2,28 @@ package com.feedback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@Configuration
 public class FeedbackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FeedbackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FeedbackApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
