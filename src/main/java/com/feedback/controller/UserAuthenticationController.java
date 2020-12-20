aae268f2c169262fea6d2e2fba3abf4033211e06
IrynaKr <yaremchukira17@gmail.com>
2020-12-21 01:13:03 +0200

    added (security)registration/login with confirmation via email

--- src/main/java/com/feedback/controller/UserAuthenticationController.java
+++ src/main/java/com/feedback/controller/UserAuthenticationController.java
@@ -7,7 +7,6 @@ import com.feedback.repo.entity.User;
 import com.feedback.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
