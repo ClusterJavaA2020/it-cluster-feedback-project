5fe750dc13414456b3bbd317e9801f04807232ce
Alex <sasha.school.2005@gmail.com>
2021-02-22 10:55:47 +0200

    schedule

--- src/main/java/com/feedback/repo/FeedbackRequestRepo.java
+++ src/main/java/com/feedback/repo/FeedbackRequestRepo.java
@@ -2,9 +2,6 @@ package com.feedback.repo;
 
 import com.feedback.repo.entity.FeedbackRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Set;
