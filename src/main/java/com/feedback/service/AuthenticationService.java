97949f34f1051d9d54e96b819fe7d51b0947b21a
javatrap2020 <javatrap2020@gmail.com>
2021-03-02 17:30:34 +0200

    ip

--- src/main/java/com/feedback/service/AuthenticationService.java
+++ src/main/java/com/feedback/service/AuthenticationService.java
@@ -3,11 +3,12 @@ package com.feedback.service;
 import com.feedback.dto.UserDto;
 import com.feedback.repo.entity.User;
 
+import java.net.UnknownHostException;
 import java.util.Optional;
 
 public interface AuthenticationService {
     UserDto register(UserDto userDto);
     Optional<User> findByEmail(String email);
     void confirmEmail(String id);
-    void sendQuestionnaire(User user);
+    void sendQuestionnaire(User user) throws UnknownHostException;
 }
--- src/main/java/com/feedback/service/AuthenticationServiceImpl.java
+++ src/main/java/com/feedback/service/AuthenticationServiceImpl.java
@@ -5,13 +5,17 @@ import com.feedback.exceptions.UserAlreadyExistException;
 import com.feedback.exceptions.UserNotFoundException;
 import com.feedback.repo.UserRepo;
 import com.feedback.repo.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.hashids.Hashids;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserRepo userRepo;
@@ -32,7 +36,12 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         }
 
         User user = userRepo.saveAndFlush(UserDto.map(userDto, passwordEncoder.encode(userDto.getPassword())));
-        sendRegistrationEmail(user);
+        try {
+            sendRegistrationEmail(user);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            log.error("error has occurred");
+        }
         return UserDto.map(user);
     }
 
@@ -47,7 +56,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         userRepo.save(user);
     }
 
-    private void sendRegistrationEmail(User user) {
+    private void sendRegistrationEmail(User user) throws UnknownHostException {
         final SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         Hashids hashids = new Hashids(SECRET_WORD);
         String id = hashids.encodeHex(user.getId().toString());
@@ -55,19 +64,35 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         simpleMailMessage.setTo(user.getEmail());
         simpleMailMessage.setSubject("You are almost registered!");
         simpleMailMessage.setFrom("feedbackapplication.mail@gmail.com");
-        simpleMailMessage.setText("Please click on the below link to activate your account. Thank you!" + "http://localhost:8080/api/auth/register/confirm/" + id);
+
+
+
+
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        //System.out.println("IP Address:- " + inetAddress.getHostAddress());
+        System.out.println("IP Address:- " + inetAddress.getLocalHost());
+
+
+        //simpleMailMessage.setText("Please click on the below link to activate your account. Thank you!" + inetAddress.getHostAddress() + "/api/auth/register/confirm/" + id);
+        simpleMailMessage.setText("Please click on the below link to activate your account. Thank you!" + inetAddress.getLocalHost() + "/api/auth/register/confirm/" + id);
+        //simpleMailMessage.setText("Please click on the below link to activate your account. Thank you!" + "http://feedback-server:8080/api/auth/register/confirm/" + id);
         emailSenderService.sendEmail(simpleMailMessage);
     }
 
     @Override
-    public void sendQuestionnaire(User user) {
+    public void sendQuestionnaire(User user) throws UnknownHostException {
         final SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setTo(user.getEmail());
         simpleMailMessage.setSubject("form");
         simpleMailMessage.setFrom("feedbackapplication.mail@gmail.com");
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        System.out.println("IP Address:- " + inetAddress.getHostAddress());
+
+
+
         //user page is in process
-        //simpleMailMessage.setText("please respond on a small questionnaire " + "http://localhost:8080/api/auth/findUserById/" + user.getId());
-        simpleMailMessage.setText("please respond on a small questionnaire " + "http://feedback-server:8080/api/auth/findUserById/" + user.getId());
+        simpleMailMessage.setText("please respond on a small questionnaire " + inetAddress.getHostAddress() + "/api/auth/findUserById/" + user.getId());
+        //simpleMailMessage.setText("please respond on a small questionnaire " + "http://feedback-server:8080/api/auth/findUserById/" + user.getId());
         emailSenderService.sendEmail(simpleMailMessage);
     }
 
